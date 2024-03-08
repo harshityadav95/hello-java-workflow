e5516d18fc4ff9e15ebc484c458f8fae94d235fb
Harshit Yadav <harshityadav@outlook.com>
2024-03-09 01:36:33 +0530

    Hello World Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String username=System.getenv("INPUT_USERNAME");
+        System.out.println("Hello"+username);
+
+
+    }
+
+
+}
