5ab3341711feb31c6339a0b4767211ae836499a3
Harshit Yadav <harshityadav@outlook.com>
2024-03-09 01:54:33 +0530

    Code Change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        String username=System.getenv("INPUT_USERNAME");
-        System.out.println("Hello"+username);
+
+        System.out.println("Hello World");
 
 
     }
